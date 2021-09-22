fb2b91fb94c44da8bda808d2a3fb2b01fba0248f
Zachary Boelter <zboelter@gmail.com>
2021-09-22 14:01:32 -0500

    js/ejs

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const drinks = require('./models/drinks.js');
 
 
 
 app.get('/', (req, res) => {
-    res.send('Welcome to the Gitpub App!')
-})
+    res.send('Welcome to the Gitpub App!');
+});
+
+app.get('/drinks', (req, res) => {
+    res.send(drinks);
+});
 
 
 
