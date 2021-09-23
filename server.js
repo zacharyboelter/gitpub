d1b2bdae33520737a8ff70591e8612896ea74c80
Zachary Boelter <zboelter@gmail.com>
2021-09-23 07:00:53 -0500

    foo bar

--- server.js
+++ server.js
@@ -3,16 +3,18 @@ const app = express();
 const port = 3000;
 const drinks = require('./models/drinks');
 
-
-
 app.get('/', (req, res) => {
-    res.render('drinks_index.ejs', { allDrinks: drinks });
+    res.send('Welcome to thee Gitpub App!')
+})
+
+app.get('/drinks', (req, res) => {
+    res.render('drinks_index.ejs', {allDrinks:drinks});
 });
 
 app.get('/drinks/:id', (req, res) => {
     // res.send(req.params.id);
-    res.render('drinks_index.ejs', {
-        drink: drinks[req.params.drinks]
+    res.render('drinks_show.ejs', {
+        drink: drinks[req.params.id]
     });
 });
 
